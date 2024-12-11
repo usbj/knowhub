05578589c162184ea29e8905f68ae59d0ac108e0
SilenceEcho <19937359196@163.com>
2024-12-11 22:40:53 +0800

    创建了项目，分化好了模块，确保项目可以启动

---
+++ rookie-admin/src/main/java/com/rookie/admin/RookieApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RookieApplication.class, args);
+    }
+
+}
