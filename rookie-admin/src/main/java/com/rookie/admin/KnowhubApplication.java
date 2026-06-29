73775e0b30c4f4911eb6e2f96c693647af91a9ed
SilenceEcho <19937359196@163.com>
2026-06-29 20:04:27 +0800

    更新knowhub知枢-知识库博客系统

--- rookie-admin/src/main/java/com/rookie/admin/KnowhubApplication.java
+++ rookie-admin/src/main/java/com/rookie/admin/KnowhubApplication.java
@@ -1,15 +1,14 @@
 package com.rookie.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.rookie"})
+@SpringBootApplication(scanBasePackages = {"com.rookie","com.knowhub"})
 //@MapperScan("com.rookie.**.mapper")
-public class RookieApplication {
+public class KnowhubApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RookieApplication.class, args);
+        SpringApplication.run(KnowhubApplication.class, args);
     }
 
 }
